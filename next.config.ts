e1a6b2cd6a1af99fb8248c7190ff8c3b23c16011
Harrsh077 <harsh.23bcy10068@gmail.com>
2025-03-23 14:42:13 +0530

    aditya first attemptz

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.icons8.com",
+        pathname: "/**",
+      },
+    ],
+  },
 };
-
-export default nextConfig;
